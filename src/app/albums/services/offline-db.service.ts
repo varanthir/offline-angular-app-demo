8c253e93c4f4b1a4ec9a714a2b4fabf9a2a041d1
Jan Strojewski <janstrojewski@evojam.com>
2019-05-09 12:12:18 +0200

    Update dependencies

--- src/app/albums/services/offline-db.service.ts
+++ src/app/albums/services/offline-db.service.ts
@@ -1,7 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+// import { openDB, IDBPDatabase } from 'idb'
+import { openDB, IDBPDatabase } from 'idb/with-async-ittr.js'
 
 @Injectable()
 export class OfflineDbService {
+  private _db: Promise<IDBPDatabase> | null = null
 
-  constructor() { }
+  public get db(): Promise<IDBPDatabase> {
+    if (this._db) {
+      return this._db
+    }
+
+    this._db = this.getDB()
+    return this._db
+  }
+
+  private getDB(): Promise<IDBPDatabase> {
+    return openDB('album-viewer', 1, {
+      upgrade(db, oldVersion /*, newVersion, transaction*/) {
+        switch (oldVersion) {
+          case 0:
+            db.createObjectStore('albums', { keyPath: 'id' })
+            db.createObjectStore('pictures')
+            db.createObjectStore('thumbnails')
+        }
+      },
+      blocked() {
+        console.log(`#idb: Connection to 'album-viewer' db is blocked`)
+      },
+      blocking() {
+        console.log(`#idb: Connection to 'album-viewer' db is blocking`)
+      }
+    })
+  }
 }
