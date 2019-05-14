02b84ea52ab2b37967ec924b4317e9962385ba0e
Jan Strojewski <janstrojewski@evojam.com>
2019-05-14 16:51:01 +0200

    Show SW info

--- src/app/service-worker/service-worker.component.ts
+++ src/app/service-worker/service-worker.component.ts
@@ -1,5 +1,7 @@
-import { Component, NgZone } from '@angular/core';
-import { ServiceWorkerFacadeService } from './state/service-worker/service-worker.facade';
+import { Component, NgZone } from '@angular/core'
+import { ServiceWorkerFacadeService } from './state/service-worker/service-worker.facade'
+import { SwUpdate } from '@angular/service-worker'
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-service-worker',
@@ -7,11 +9,18 @@ import { ServiceWorkerFacadeService } from './state/service-worker/service-worke
   styleUrls: ['./service-worker.component.scss']
 })
 export class ServiceWorkerComponent {
+  public readonly updateAvailable$ = this.swUpdate.available.pipe(
+    map(obj => JSON.stringify(obj, null, 2)))
+
+  public get swIsEnabled(): boolean {
+    return this.swUpdate.isEnabled
+  }
 
   constructor(
     private readonly ngZone: NgZone,
     private readonly serviceWorkerFacade: ServiceWorkerFacadeService,
-    ) { }
+    private readonly swUpdate: SwUpdate,
+  ) {}
 
   public toggleIsStable(): void {
     this.serviceWorkerFacade.toggleShowIsStable()
@@ -29,5 +38,9 @@ export class ServiceWorkerComponent {
         console.log('NgZone.isInAngularZone() =>', NgZone.isInAngularZone())
       }, 2000)
     })
+  }
+
+  public checkForUpdate(): void {
+    this.swUpdate.checkForUpdate().then(() => {}).catch(() => {})
   }
 }
