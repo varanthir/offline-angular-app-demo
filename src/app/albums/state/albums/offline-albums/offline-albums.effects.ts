4f850880af6e508e72668f10aed7340116a9c045
Jan Strojewski <janstrojewski@evojam.com>
2020-04-16 16:29:48 +0200

    Fix - dont load unfinished offline albums

--- src/app/albums/state/albums/offline-albums/offline-albums.effects.ts
+++ src/app/albums/state/albums/offline-albums/offline-albums.effects.ts
@@ -1,7 +1,7 @@
 import { Action } from '@ngrx/store'
 import { Injectable } from '@angular/core'
 import { Actions, Effect, ofType } from '@ngrx/effects'
-import { Observable, of } from 'rxjs'
+import { Observable, of, forkJoin } from 'rxjs'
 import {
   OfflineAlbumsActionTypes,
   GetOfflineAlbumsSuccessAction,
@@ -17,6 +17,17 @@ import { catchError, map, switchMap } from 'rxjs/operators'
 import { AlbumsStorageService } from '../../dal/dao/albums.storage'
 import { GetOfflineFilesUrlsAction } from '../offline-files/offline-files.actions';
 import { GetStorageEstimateAction } from '../../storage-estimate/storage-estimate.actions';
+import { AlbumsFinishedStorageService } from '../../dal/dao/albums-finished.storage'
+import { AlbumFinished } from '../../dal/dao/album-viewer-db.service'
+import { Album } from '../../dal/dto/album'
+
+const finishedAlbums = ([albumsFinished, albums]: [AlbumFinished[], Album[]]) => {
+  const filteredIds = albumsFinished
+    .filter(({ isFinished }) => isFinished)
+    .map(({ id }) => id)
+
+  return albums.filter(album => filteredIds.includes(album.id))
+}
 
 @Injectable()
 export class OfflineAlbumsEffects {
@@ -24,10 +35,15 @@ export class OfflineAlbumsEffects {
   @Effect()
   readonly getAlbums$: Observable<Action> = this.actions$.pipe(
     ofType(OfflineAlbumsActionTypes.GET_OFFLINE_ALBUMS),
-    switchMap(() => this.albumsStorage.getAll().pipe(
-      map(albums => new GetOfflineAlbumsSuccessAction({ albums })),
-      catchError((error: Error) => of(new GetOfflineAlbumsErrorAction(error)))
-    ))
+    switchMap(() => forkJoin([
+      this.albumsFinishedStorage.getAll(),
+      this.albumsStorage.getAll()
+    ]).pipe(
+        map(finishedAlbums),
+        map(albums => new GetOfflineAlbumsSuccessAction({ albums })),
+        catchError((error: Error) => of(new GetOfflineAlbumsErrorAction(error)))
+      )
+    )
   )
 
   @Effect()
@@ -75,5 +91,6 @@ export class OfflineAlbumsEffects {
   constructor(
     private readonly actions$: Actions<OfflineAlbumsActions>,
     private readonly albumsStorage: AlbumsStorageService,
+    private readonly albumsFinishedStorage: AlbumsFinishedStorageService,
   ) {}
 }
